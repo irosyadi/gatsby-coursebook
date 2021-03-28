fc22f178d1d68bffb6f2a5b22dad4c841f9aa7e5
Imron Rosyadi <mas.iyom@gmail.com>
2021-03-29 06:54:18 +0700

    initial upload

---
+++ gatsby-meta-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  title: `Imron Rosyadi`,
+  description: `a desert traveler`,
+  author: `Imron Rosyadi`,
+  introduction: `notes of a life`,
+  siteUrl: `https://irosyadi.netlify.app/`, // Your blog site url
+  social: {
+    twitter: `irosyadi`, // Your Twitter account
+    github: `irosyadi`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, 	
+    //facebook: `imron.rosyadi`, // Your Facebook account
+    linkedin: `irosyadi`, // Your LinkedIn account
+  },
+  icon: `content/assets/icon.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    utterances: "", // Your repository for archive comment
+//    utterances: "irosyadi/gatsby-site", // Your repository for archive comment	
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: "irosyadi",
+  },
+  share: {
+//    facebookAppId: "", // Add facebookAppId for using facebook share feature v3.2	  
+    facebookAppId: "401609837854601", // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: "UA-185820309-1", // Add your google analytics tracking ID
+  // jennifer_front_id: "ed243e67", // Add your jennifer front tracking ID
+}
